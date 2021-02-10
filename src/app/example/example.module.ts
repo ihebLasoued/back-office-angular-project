b8c8cf23ad2e7ad422d8c45d0bc42214d6994a8a
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2021-02-10 14:31:47 +0100

    update version

--- src/app/example/example.module.ts
+++ src/app/example/example.module.ts
@@ -6,8 +6,11 @@ import {ExampleRouting} from "./example.routing";
 import { ProduitComponent } from './produit/produit.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {  MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,
-  MatSortModule, MatTableModule } from "@angular/material";
+import { MatInputModule } from "@angular/material/input";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
   import { MatIconModule } from '@angular/material/icon';
   import {MatButtonModule} from '@angular/material/button';
 import { UserComponent } from './user/user.component';
